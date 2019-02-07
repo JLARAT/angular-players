0d81307b6fcb96e46a81f2b2ddb606adf5c3fb24
Jocelyn LARAT <jocelyn.larat@gmail.com>
2019-02-07 14:57:16 +0100

    Adding routing module

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,24 +3,21 @@ import { Player } from '../player';
 import { PlayerService } from '../player.service';
 
 @Component({
-  selector: 'app-players',
-  templateUrl: './players.component.html',
-  styleUrls: ['./players.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class PlayersComponent implements OnInit {
-
-  players : Player[];
+export class DashboardComponent implements OnInit {
+  players: Player[] = [];
 
   constructor(private playerService: PlayerService) { }
-  
+
   ngOnInit() {
     this.getPlayers();
   }
 
-
   getPlayers(): void {
     this.playerService.getPlayers()
-        .subscribe(players_f => this.players = players_f);
+      .subscribe(players => this.players = players.slice(1, 5));
   }
-
 }
