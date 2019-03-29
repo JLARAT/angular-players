e049314cd3966b964d6d42b7f1596137015757d2
Jocelyn LARAT <jocelyn.larat@gmail.com>
2019-03-29 11:34:29 +0100

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,14 @@ import { PlayerDetailComponent }  from './player-detail/player-detail.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'players', component: PlayersComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: PlayerDetailComponent },
-  { path: 'players', component: PlayersComponent }
-  
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
-export class AppRoutingModule {
-  
+export class AppRoutingModule {}
 
-}
